7d89ab94221e9df5d07c38c921d7f5d5ff6280f4
Lorraine Thomas <thomas.lorraine1@gmail.com>
2024-03-29 17:36:14 -0400

    Added search functionality with axios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+function convertTemperature(celcius){
+    let temp = 9/5 * celcius + 32;
+    return temp;
+}
+
+function refreshWeather(response){
+    let temperatureElement = document.querySelector("#temperature");
+    let temperatureValue = Math.round(convertTemperature(response.data.temperature.current));
+    temperatureElement.innerHTML = temperatureValue;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city){
+let apiKey = "73a673doafc73bccb4ff04ct60632486";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
