9e1ca615d42370f23333d4add72b60a24f299d6b
Lorraine Thomas <thomas.lorraine1@gmail.com>
2024-03-30 23:45:32 -0400

    Added functionality to update day and time

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-function convertTemperature(celcius){
-    let temp = 9/5 * celcius + 32;
-    return temp;
-}
-
 function refreshWeather(response){
     let temperatureElement = document.querySelector("#temperature");
     let temperatureValue = Math.round(convertTemperature(response.data.temperature.current));
@@ -21,13 +16,25 @@ function refreshWeather(response){
     let speed = Math.round(response.data.wind.speed * 1.609344);
     windElement.innerHTML = speed;
 
-    //timeElement 16:32
+    let dayTime = new Date(response.data.time * 1000);
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    timeElement = document.querySelector("#time");
+    let minutes = dayTime.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${dayTime.getMinutes()}`;
+    }
+    timeElement.innerHTML = `${days[dayTime.getDay()]} ${dayTime.getHours()}:${minutes}`;
 }
 
 function searchCity(city){
 let apiKey = "73a673doafc73bccb4ff04ct60632486";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(refreshWeather);
+}
+
+function convertTemperature(celcius){
+    let temp = 9/5 * celcius + 32;
+    return temp;
 }
 
 function handleSearchSubmit(event){
